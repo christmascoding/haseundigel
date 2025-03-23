ad9a1477e95ede5060add7a6cba708a991efbf83
Heinkel162 <36361643+Heinkel162@users.noreply.github.com>
2025-03-23 20:58:47 +0100

    adds game classes and working Aktionskartenstapel

---
+++ src/Config.java
@@ -0,0 +1,7 @@
+package src;
+
+public interface Config {
+
+    public static int [] initKarotten = {68, 98}; // Startwert Karotten eines jeden Spielers
+    public static int initSalate = 3; // Startwert Salate eines jeden Spielers
+}
